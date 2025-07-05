06deb1e05e5875cbf446868656af6eeb2738301b
byronncat <anhthinhncat@gmail.com>
2025-07-05 22:52:10 +0700

    fix: case sensitive folder

--- src/types/convention.d.ts
+++ src/types/convention.d.ts
@@ -10,3 +10,58 @@ declare module "*.mp4" {
   const content: string;
   export default content;
 }
+
+// Next.js image
+interface StaticImageData {
+  src: string;
+  height: number;
+  width: number;
+  blurDataURL?: string;
+  blurWidth?: number;
+  blurHeight?: number;
+}
+
+declare module "*.avif" {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module "*.webp" {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module "*.gif" {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module "*.bmp" {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module "*.ico" {
+  const content: StaticImageData;
+  export default content;
+}
