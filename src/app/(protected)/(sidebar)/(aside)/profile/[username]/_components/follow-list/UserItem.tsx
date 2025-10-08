2f570d3c4b27d116ebaa54ed4dffd9ac41e1946f
byronncat <anhthinhncat@gmail.com>
2025-10-08 12:49:10 +0700

    fix: element condition

--- src/app/(protected)/(sidebar)/(aside)/profile/[username]/_components/follow-list/UserItem.tsx
+++ src/app/(protected)/(sidebar)/(aside)/profile/[username]/_components/follow-list/UserItem.tsx
@@ -84,7 +84,7 @@ export default function UserItem({ data, onFollow }: UserItemProps) {
           />
         </div>
 
-        <p className="text-sm mt-1.5">{data.bio}</p>
+        {data.bio && <p className="text-sm mt-1.5">{data.bio}</p>}
       </div>
     </Link>
   );
