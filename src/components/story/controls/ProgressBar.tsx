47cdcade27a2d3878c217863ca9696ddcd174c80
byronncat <anhthinhncat@gmail.com>
2025-11-20 18:19:11 +0700

    refactor: prevent navigate at current progress bar

--- src/components/story/controls/ProgressBar.tsx
+++ src/components/story/controls/ProgressBar.tsx
@@ -17,35 +17,41 @@ export default function ProgressBar({
 }: ProgressBarProps) {
   return (
     <div className={cn("flex gap-1", className)}>
-      {Array.from({ length: total }).map((_, index) => (
-        <div
-          // eslint-disable-next-line react/no-array-index-key
-          key={index}
-          className={cn(
-            "bg-white/40",
-            "flex-1 h-1",
-            "rounded-full overflow-hidden",
-            "cursor-pointer"
-          )}
-          onClick={() => onSegmentClick(index)}
-        >
+      {Array.from({ length: total }).map((_, index) => {
+        const isCurrent = index === current;
+
+        return (
           <div
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
             className={cn(
-              "bg-white",
-              "h-full rounded-full",
-              "transition-all duration-75 ease-linear"
+              "bg-white/40",
+              "flex-1 h-1",
+              "rounded-full overflow-hidden",
+              !isCurrent && "cursor-pointer"
             )}
-            style={{
-              width:
-                index < current
-                  ? "100%"
-                  : index === current
-                    ? `${progress}%`
-                    : "0%",
+            onClick={() => {
+              if (isCurrent) return;
+              onSegmentClick(index);
             }}
-          />
-        </div>
-      ))}
+          >
+            <div
+              className={cn(
+                "bg-white",
+                "h-full rounded-full",
+                "transition-all duration-75 ease-linear"
+              )}
+              style={{
+                width: isCurrent
+                  ? `${progress}%`
+                  : index < current
+                    ? "100%"
+                    : "0%",
+              }}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
